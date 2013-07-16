5bd02d47963deb66f499d67e58c4797298f4f9b6
dosjia <dosjia@163.com>
2013-07-16 21:48:00 +0800

    add AFNETWorking

--- Podfile
+++ Podfile
@@ -1,2 +1,3 @@
 platform :ios
 pod 'JSONKit'
+pod 'AFNetworking', '0.9.1'
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -6,6 +6,12 @@
 // project.
 
 
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 0
+#define COCOAPODS_VERSION_MINOR_AFNetworking 9
+#define COCOAPODS_VERSION_PATCH_AFNetworking 1
+
 // JSONKit
 #define COCOAPODS_POD_AVAILABLE_JSONKit
 // This library does not follow semantic-versioning,
